214bd704256de94e85e7141f3ebf32d4f210f1c9
Pedro Henrique Ribeiro <pedro.ribeiro33@fatec.sp.gov.br>
2024-03-07 17:22:04 -0300

    finalização da aula do dia 07/03/24

--- Gerson-PO/typescript/07-03-24/calculando/out/index.js
+++ Gerson-PO/typescript/07-03-24/calculando/out/index.js
@@ -3,6 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+var multiplicacao_1 = __importDefault(require("./multiplicacao"));
 var soma_1 = __importDefault(require("./soma"));
+var subtracao_1 = __importDefault(require("./subtracao"));
 var calculo = new soma_1.default();
 console.log(calculo.calcular(10, 5));
+calculo = new multiplicacao_1.default();
+console.log(calculo.calcular(2, 4));
+calculo = new subtracao_1.default();
+console.log(calculo.calcular(21, 4));
--- Gerson-PO/typescript/07-03-24/calculando/src/index.ts
+++ Gerson-PO/typescript/07-03-24/calculando/src/index.ts
@@ -1,4 +1,12 @@
+import multiplicacao from "./multiplicacao"
 import Soma from "./soma"
+import Subtracao from "./subtracao"
 
 let calculo = new Soma()
 console.log(calculo.calcular(10,5))
+
+calculo = new multiplicacao()
+console.log(calculo.calcular(2,4))
+
+calculo = new Subtracao()
+console.log(calculo.calcular(21,4))
